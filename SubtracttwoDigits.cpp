be67bafc06d265a3fb8a421277fa680c90ff7eea
Abhimanyu0042 <abhimanyu0042.be21@chitkara.edu.in>
2022-04-07 23:31:18 +0530

    C++ Program

---
+++ SubtracttwoDigits.cpp
@@ -0,0 +1,13 @@
+//WAP a program to find the difference of two numbers.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, D;
+    cout << "Enter the two numbers :-";
+    cin>>a>>b ;
+    D = a - b ;
+    cout << "The difference of two numbers is:" << D <<endl ;
+    return 0;
+}
