0bb8b916c5a838ad5ef194dfd9006b5ba58ea9b3
Abhimanyu0042 <abhimanyu0042.be21@chitkara.edu.in>
2022-05-06 15:43:35 +0530

    C++ Program

---
+++ Operator Overloading/Matrix(AddMultiplication).cpp
@@ -0,0 +1,87 @@
+
+#include <iostream>
+using namespace std;
+
+class Matrix
+{
+  int mat[3][3];
+public:
+  void set ()
+  {
+    int i, j;
+    for (i = 0; i <= 2; i++)
+      {
+	for (j = 0; j <= 2; j++)
+	  {
+	    cin >> mat[i][j];
+	  }
+      }
+  }
+  Matrix operator+ (Matrix m2)
+  {
+    int i, j;
+    Matrix m3;
+    for (i = 0; i <= 2; i++)
+      {
+	for (j = 0; j <= 2; j++)
+	  {
+	    m3.mat[i][j] = mat[i][j] + m2.mat[i][j];
+	  }
+      }
+    return m3;
+  }
+  
+  //For Multiplying Matrix
+    Matrix operator* (Matrix m2)
+  {
+    int i, j;
+    Matrix m3;
+    for (i = 0; i <= 2; i++)
+      {
+	for (j = 0; j <= 2; j++)
+	  {
+	    m3.mat[i][j] = mat[i][j]*m2.mat[i][j];
+	  }
+      }
+    return m3;
+  }
+    Matrix operator- (Matrix m3)
+  {
+    int i, j;
+    Matrix m4;
+    for (i = 0; i <= 2; i++)
+      {
+	for (j = 0; j <= 2; j++)
+	  {
+	    m4.mat[i][j] = mat[i][j] - m3.mat[i][j];
+	  }
+      }
+    return m4;
+  }
+  
+  
+  void display ()
+  {
+    int i, j;
+    for (i = 0; i <= 2; i++)
+      {
+	for (j = 0; j <= 2; j++)
+	  {
+	    cout << mat[i][j] << " ";
+	  }
+	cout << endl;
+      }
+  }
+};
+
+int
+main ()
+{
+  Matrix m1, m2, m3, m4;
+  m1.set ();
+  m2.set ();
+  //m3 = m1 + m2;
+  m4 = m1 - m1*m2;
+  m4.display();
+  return 0;
+}
