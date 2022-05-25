23ce7aa44a74d63f671e6c85d45217844dd69b54
Abhimanyu0042 <abhimanyu0042.be21@chitkara.edu.in>
2022-05-25 10:15:35 +0530

    C++ Program

---
+++ Template/FunctionTemplate.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int sum(int n1,int n2)
+{return n1+n2;}
+float sum(float n1, float n2)
+{return n1+n2;}
+double sum(double n1, double n2)
+{return n1+n2;}
+
+template <class T>
+T sum(T n1, T n2)
+{return n1+n2;}
+int main()
+{
+    int a, b, c;
+    float x, y, z;
+    double i, j, k;
+    cin>>a>>b;
+    cout<<sum(a,b)<<endl;
+    cin>>x>>y;
+    cout<<sum(x,y)<<endl;
+    cin>>i>>j;
+    cout<<sum(i,j);
+    // cout<<sum(a,i); it will be error
+    return 0;
+}
